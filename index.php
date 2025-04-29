241ee0d1afdd7adc3d60feac1469f7385df8558b
HablasBlaBla <00001111987208sp@al.educacao.sp.gov.br>
2025-04-28 22:40:03 -0300

    algumas alterações na página inicial

--- index.php
+++ index.php
@@ -3,17 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página initial</title>
+    <title>Página inicial</title>
 </head>
 <style>
     body {
-        background-color: black;
+        background-color: #1C1C1C;
         color: whitesmoke;
     }
 </style>
 <body>
     <div>
         <h1>Bem-Vindo(a) à página inicial do site!</h1>
+    </div>
+    <div>
+        <h2>Para utilizar o site, por favor,  <a href="frontend/cadastro-clientes.php">faça seu cadastro</a></h2>
     </div>
 </body>
 </html>
