28bbb536181f3534fc0ecbf7b7961334e6cfad5b
HablasBlaBla <00001111987208sp@al.educacao.sp.gov.br>
2025-04-28 21:32:42 -0300

    página inicial do projeto

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página initial</title>
+</head>
+<style>
+    body {
+        background-color: black;
+        color: whitesmoke;
+    }
+</style>
+<body>
+    <div>
+        <h1>Bem-Vindo(a) à página inicial do site!</h1>
+    </div>
+</body>
+</html>
